7d27d2de232709bd445e4e39b897df6a410714ee
Mickey-Lelouch <ichigozettai@gmail.com>
2025-07-09 11:45:14 +0100

    Add files via upload

---
+++ plugins/auto-recoding.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+const config = require('../settings')
+const {LELOUCH , commands} = require('../LELOUCH')
+
+
+//auto recording
+LELOUCH({
+  on: "body"
+},    
+async (conn, mek, m, { from, body, isOwner }) => {       
+ if (config.AUTO_RECORDING === 'true') {
+                await conn.sendPresenceUpdate('recording', from);
+            }
+         } 
+   );
